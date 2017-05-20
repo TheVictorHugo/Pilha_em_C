7223e43b854a39559afd3b7ac38104c0a8760cbb
Victor Hugo <victorq32@gmail.com>
2017-05-19 22:59:51 -0300

    commit 1
    começo do projeto

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+	Name: Victor Hugo Rodrigues da Silva
+	Copyright: 
+	Author: 
+	Date: 19/05/17 22:57
+	Description: Pilha em C
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "code.h"
+
+void main(){
+	
+	
+	
+}
